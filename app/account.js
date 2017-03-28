ac0b90c150c282bff28bcfd96c5835922ef24086
Mayank Saxena <sm.mayank@icloud.com>
2017-03-28 23:33:36 +0200

    Handling 404, 500 case

--- app/account.js
+++ app/account.js
@@ -5,6 +5,17 @@ var app = express()
 app.use(logger('tiny'))
 app.use('/', require('./routes'))
 
+// route not found
+app.use(function(req, res, next) {
+    console.error(err.stack)
+    res.status(404).send('Earth is flat, and you found an edge')
+})
+
+// error case
+app.use(function(err, req, res, next) {
+    res.status(500).send('Something broke!')
+})
+
 var port = process.env.ACCOUNT_PORT || 3000
 
 global.db = require('../models')
