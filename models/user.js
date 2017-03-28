bf17043ea65a4e170090bd811eaa786317762554
Mayank Saxena <sm.mayank@icloud.com>
2017-03-29 00:09:25 +0200

    Added uuid to user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var uuidV4 = require('uuid/v4')
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     uuid: DataTypes.STRING,
@@ -11,5 +14,9 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   });
+  User.beforeCreate('addUuid', function addUuid(create, options){
+    create.uuid = uuidV4()
+    return create
+  })
   return User;
 };
